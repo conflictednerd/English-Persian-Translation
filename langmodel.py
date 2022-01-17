879460de3aaf22166884866a1046fb4f46d27601
hedayatians <hedayatians@gmail.com>
2022-01-18 03:26:13 +0330

    initial commit

---
+++ langmodel.py
@@ -0,0 +1,20 @@
+from translator import Translator
+
+class LMTranslator(Translator):
+    def __init__(self, args):
+        super().__init__()
+
+    def load(self, path: str):
+        pass
+
+    def save(self, path: str):
+        pass
+
+    def translate(self, src_txt: str):
+        pass
+
+    def train(self, args):
+        pass
+
+    def test(self, args):
+        pass
---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+from pprint import pprint
+
+from langmodel import LMTranslator
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='lm',
+                        help='type of translation model: lm')
+    parser.add_argument('--load_model', action='store_true', default=False,
+                        help='use if you want to load the model from file')
+    parser.add_argument('--train', action='store_true',
+                        default=False, help='use if you want to train the model')
+    parser.add_argument('--test', action='store_true', default=False,
+                        help='use if you want the model to report performance on a held-out test set')
+    parser.add_argument('--data_path', default='./data/',
+                        help='directory where training data is stored')
+    parser.add_argument('--models_dir', default='./models_dir/',
+                        help='directory where models are saved to/loaded from')
+
+    # Language model parameters:
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    pprint(f'Arguments are: {vars(args)}')  # For debugging
+
+    if args.model == 'lm':
+        model = LMTranslator(args)
+    else:
+        raise ValueError(f'Model "{args.model}" is not supported')
+
+    if args.train:
+        model.train(args)
+
+    if args.test:
+        model.test(args)
+
+    while True:
+        print(model.translate(input()))
